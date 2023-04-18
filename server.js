f1e578a97e67457d4c4cecccbabe02822f4a1250
LinxPhy <lin.aboagye@gmail.com>
2023-04-18 23:38:05 +0100

    third

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, 'src')))
 app.use(cors(
     {
-        origin: 'http://https://youranimetowatch.netlify.app/',
+        origin: 'http://youranimetowatch.netlify.app/',
         method: ['GET', 'POST'],
     }
 ));
